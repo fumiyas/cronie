f4ff29d9a196338ef8faeaa83a2f6e4708a0c414
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2014-04-23 17:54:18 +0900

    AIX: crond: Do not use setenv(3) and unsetenv(3) in the main loop
    AIX cannot support $CRON_TZ because it does not free environment
    on setenv(3) and unsetenv(3).

--- src/cron.c
+++ src/cron.c
@@ -520,39 +520,11 @@ static void run_reboot_jobs(cron_db * db) {
 }
 
 static void find_jobs(int vtime, cron_db * db, int doWild, int doNonWild, long vGMToff) {
-	char *orig_tz, *job_tz;
 	struct tm *tm;
 	int minute, hour, dom, month, dow;
 	user *u;
 	entry *e;
 	const char *uname;
-
-	/* The support for the job-specific timezones is not perfect. There will
-	 * be jobs missed or run twice during the DST change in the job timezone.
-	 * It is recommended not to schedule any jobs during the hour when
-	 * the DST changes happen if job-specific timezones are used.
-	 *
-	 * Make 0-based values out of tm values so we can use them as indicies
-	 */
-#define maketime(tz1, tz2) do { \
-	char *t = tz1; \
-	if (t != NULL && *t != '\0') { \
-		setenv("TZ", t, 1); \
-		tm = localtime(&virtualGMTSecond); \
-	} else { if ((tz2) != NULL) \
-			setenv("TZ", (tz2), 1); \
-		else \
-			unsetenv("TZ"); \
-		tm = gmtime(&virtualSecond); \
-	} \
-	minute = tm->tm_min -FIRST_MINUTE; \
-	hour = tm->tm_hour -FIRST_HOUR; \
-	dom = tm->tm_mday -FIRST_DOM; \
-	month = tm->tm_mon +1 /* 0..11 -> 1..12 */ -FIRST_MONTH; \
-	dow = tm->tm_wday -FIRST_DOW; \
-	} while (0)
-
-	orig_tz = getenv("TZ");
 
 		/* the dom/dow situation is odd.  '* * 1,15 * Sun' will run on the
 		 * first and fifteenth AND every Sunday;  '* * * * Sun' will run *only*
@@ -570,8 +542,12 @@ static void find_jobs(int vtime, cron_db * db, int doWild, int doNonWild, long v
 			if (getpwnam(uname) != NULL) {
 				time_t virtualSecond = (vtime - e->delay) * SECONDS_PER_MINUTE;
 				time_t virtualGMTSecond = virtualSecond - vGMToff;
-				job_tz = env_get("CRON_TZ", e->envp);
-				maketime(job_tz, orig_tz);
+				tm = localtime(&virtualGMTSecond);
+				minute = tm->tm_min -FIRST_MINUTE;
+				hour = tm->tm_hour -FIRST_HOUR;
+				dom = tm->tm_mday -FIRST_DOM;
+				month = tm->tm_mon +1 /* 0..11 -> 1..12 */ -FIRST_MONTH;
+				dow = tm->tm_wday -FIRST_DOW;
 				/* here we test whether time is NOW */
 				if (bit_test(e->minute, minute) &&
 					bit_test(e->hour, hour) &&
@@ -581,12 +557,6 @@ static void find_jobs(int vtime, cron_db * db, int doWild, int doNonWild, long v
 						: (bit_test(e->dow, dow) || bit_test(e->dom, dom))
 					)
 					) {
-					if (job_tz != NULL && vGMToff != GMToff)
-						/* do not try to run the jobs from different timezones
-						 * during the DST switch of the default timezone.
-						 */
-						continue;
-
 					if ((doNonWild &&
 							!(e->flags & (MIN_STAR | HR_STAR))) ||
 						(doWild && (e->flags & (MIN_STAR | HR_STAR))))
@@ -595,10 +565,6 @@ static void find_jobs(int vtime, cron_db * db, int doWild, int doNonWild, long v
 			}
 		}
 	}
-	if (orig_tz != NULL)
-		setenv("TZ", orig_tz, 1);
-	else
-		unsetenv("TZ");
 }
 
 /*
